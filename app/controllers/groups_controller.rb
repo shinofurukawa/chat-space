ce8ab1a118aea3228478703b430234215205b2c3
shino <shinoppa220@gmail.com>
2018-11-12 21:41:57 +0900

    Fix groups#create to add current_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,12 +10,14 @@ class GroupsController < ApplicationController
 
     def create
       @group = Group.new(group_params)
+      @group.users << current_user
       if @group.save
         redirect_to root_path, notice: "グループを作成しました"
       else
         render :new
       end
     end
+
     def update
       if @group.update(group_params)
         redirect_to group_messages_path(@group), notice: 'グループを編集しました'
