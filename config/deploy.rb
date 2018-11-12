0d87d4268335e4694cabab5a526bb1864f783ca2
shino <shinoppa220@gmail.com>
2018-11-12 20:25:15 +0900

    key_rename

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<jre2016>']
+                  keys: ['/Users/tech-camp/.ssh/jre2016.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
