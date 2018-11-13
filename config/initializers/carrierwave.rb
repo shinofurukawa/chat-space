b6e382625418b447d6056f564b1ea2be80471795
shino <shinoppa220@gmail.com>
2018-11-13 18:46:26 +0900

    fix_region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: '※自分で調べて入れてください'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'update.chat-space'
-  config.asset_host = 'https://s3-us-east-1.amazonaws.com/update.chat-space'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/update.chat-space'
 end
